2a3440517b741ca2a2461533a56382a4ad72c0da
weakchicken <1481723905@qq.com>
2019-06-10 22:14:16 +0800

    Site updated: 2019-06-10 22:14:03

--- js/schemes/muse.js
+++ js/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Sun Jun 09 2019 16:13:02 GMT+0800 (GMT+08:00)
+// build time:Mon Jun 10 2019 22:13:50 GMT+0800 (GMT+08:00)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
