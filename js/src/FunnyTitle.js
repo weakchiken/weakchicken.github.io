caee8613cc5aa094b2de65444cc980f849aa776a
weakchicken <1481723905@qq.com>
2019-07-21 09:43:57 +0800

    Site updated: 2019-07-21 09:43:43

---
+++ js/src/FunnyTitle.js
@@ -0,0 +1,17 @@
+<!--浏览器搞笑标题-->
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = 'ヽ(●-`Д´-)ノ你走了就别回来了！！！';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = 'ヾ(Ő∀Ő3)ノ很高兴看到你回来了~~' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
